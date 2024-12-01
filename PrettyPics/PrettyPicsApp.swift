ecd84597780b3efc5b605fe1d43f994b6ed13527
PJ <pjloury@gmail.com>
2024-12-01 11:02:13 -0800

    Initial Commit

---
+++ PrettyPics/PrettyPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrettyPicsApp.swift
+//  PrettyPics
+//
+//  Created by PJ Loury on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PrettyPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrettyPicsTests/PrettyPicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrettyPicsTests.swift
+//  PrettyPicsTests
+//
+//  Created by PJ Loury on 12/1/24.
+//
+
+import Testing
+@testable import PrettyPics
+
+struct PrettyPicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
